bb80e085b808b6fd084949b14024eb63df024001
KhoiPN <khoipn.dev@gmail.com>
2019-07-22 04:39:52 -0700

    Init helloword

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+var hostname = 'localhost';
+var port = 3000;
+
+app.get('/helloword', function (req, res) {
+    res.send(`Hello World !!!`)
+});
+
+app.listen(port, hostname, () => console.log(`Server running on port: ${port}`));
